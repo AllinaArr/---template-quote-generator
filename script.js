03625c7993f7a20821cbdc9f2fe99872879de18d
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2023-10-26 12:53:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+    } catch (error) {
+        //Catch Error Here
+    }
+}
